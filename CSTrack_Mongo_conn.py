f1033837aea9cb8929623247a4b1fbf1356ea7cf
mcalvera <61874057+mcalvera@users.noreply.github.com>
2020-12-04 10:02:01 +0100

    New information
    New information

--- CSTrack_Mongo_conn.py
+++ CSTrack_Mongo_conn.py
@@ -12,14 +12,6 @@ def connection():
         #port
         port = config.PORTDEV #8327 = prod
         port_prod = config.PORTPROD
-        
-        #LOCALHOST
-        #conn = MongoClient('mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false') #localhost
-        #PROD
-        #self.conn = MongoClient('mongodb://root:g9k.LS00-1!JbzA8..@internal-docker.sb.upf.edu:8327/?readPreference=primary&appname=MongoDB%20Compass&ssl=false')
-        #TEST
-        #conn = MongoClient('mongodb://root:g9k.LS00-1!JbzA8..@internal-docker.sb.upf.edu:8336/?readPreference=primary&appname=MongoDB%20Compass&ssl=false')
 
         connection =  'mongodb://' + user + ':' + password + '@' + host + ':' + port + connection_str
         return connection
-
